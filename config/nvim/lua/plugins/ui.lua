bc66f6aef2078f0be77d23f82e74c7dea6bd93f2
ricci2511 <ricardo.christmann@protonmail.com>
2024-02-03 01:02:00 +0100

    nvim: add custom dashboard

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -5,4 +5,21 @@ return {
       opts.presets.lsp_doc_border = true
     end,
   },
+  {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    opts = function(_, opts)
+      local logo = [[
+         _           _ ___   ____________
+   _____(_)_________(_)__ \ / ____<  <  /
+  / ___/ / ___/ ___/ /__/ //___ \ / // / 
+ / /  / / /__/ /__/ // __/____/ // // /  
+/_/  /_/\___/\___/_//____/_____//_//_/   
+                                         
+      ]]
+
+      logo = string.rep("\n", 8) .. logo .. "\n\n"
+      opts.config.header = vim.split(logo, "\n")
+    end,
+  },
 }
