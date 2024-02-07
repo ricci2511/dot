af9215b0e2fadc195dcfab20d2fb94eba382b0b6
ricci2511 <ricardo.christmann@protonmail.com>
2024-02-07 22:41:51 +0100

    fix wezterm path

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,39 @@
+local wezterm = require("wezterm")
+
+local is_windows = wezterm.target_triple == "x86_64-pc-windows-msvc"
+
+return {
+	color_scheme = "Dracula",
+	font = wezterm.font("FiraCode Nerd Font", { weight = 400 }),
+	font_size = 13.0,
+	window_background_opacity = 0.90,
+	macos_window_background_blur = 30,
+	enable_tab_bar = false,
+	default_cursor_style = "BlinkingUnderline",
+	cursor_blink_rate = 600,
+
+	-- window_background_image_hsb = {
+	-- 	brightness = 0.01,
+	-- 	hue = 1.0,
+	-- 	saturation = 0.3,
+	-- },
+
+	-- Arch wsl as default domain on windows
+	default_domain = is_windows and "WSL:Arch" or nil,
+
+	keys = {
+		{
+			key = "f",
+			mods = "CTRL",
+			action = wezterm.action.ToggleFullScreen,
+		},
+	},
+	mouse_bindings = {
+		-- Ctrl-click to open links
+		{
+			event = { Up = { streak = 1, button = "Left" } },
+			mods = "CTRL",
+			action = wezterm.action.OpenLinkAtMouseCursor,
+		},
+	},
+}
