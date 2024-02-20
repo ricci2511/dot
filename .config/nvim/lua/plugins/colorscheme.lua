0d1570dff0527aeb0a679fe89def03eb90095aed
ricci2511 <rc@umh.app>
2024-02-20 17:15:44 +0100

    update nvim plugins and set theme options

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,21 @@
 return {
   { "Mofiqul/dracula.nvim" },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "catppuccin/nvim",     name = "catppuccin", priority = 1000 },
+  {
+    "folke/tokyonight.nvim",
+    name = "tokyonight",
+    opts = {
+      style = "night",
+      styles = {
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments = { italic = true },
+        keywords = { italic = false },
+        sidebars = "dark",
+        floats = "dark",
+      },
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
